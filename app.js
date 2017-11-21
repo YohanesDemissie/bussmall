a9e99bef678639e6c3454e07850d1963b077d7cc
Yohanes Demissie <Yohanesdemissie@gmail.com>
2017-11-20 22:27:17 -0800

    hard code image for inspiration

--- app.js
+++ app.js
@@ -41,11 +41,12 @@ var productRank = {
   displayImages: function() {
     var num = Math.floor(Math.random());
      document.getElementById (Array [num]);
-    // TODO: Hmm... what's going to happen here?
+    // TODO: Hmm... what's going to happen here?//
 var getNum = document.getElementById('num');
   getNum.textContent = num;
   },
-updateMessage()l; //I used the above variable getNum and code from example from page 89 in javascript book
+updateMessage();
+ //I used the above variable getNum and code from example from page 89 in javascript book
   tallyClicks: function(elementId) {
     // TODO: Hmm... what's going to happen here?
     totalClicks++;
@@ -70,41 +71,41 @@ productRank.imageEls.addEventListener('click', productRank.onClick);
 productRank.displayImages();
 
 //use strict
-'use strict';
-var stores = [];
-
-var hours = ['6:00 AM', '7:00 AM', '8:00 AM', '9:00 AM', '10:00 AM','11:00 AM','12:00 PM','1:00 PM','2:00 PM','3:00 PM','4:00 PM','5:00 PM','6:00 PM','7:00 PM','8:00 PM','Total'];
-
-function Store (id, min, max, avg){
-  this.id = id;
-  this.min = min;
-  this.max = max;
-  this.avg = avg;
-  this.hourlyCookies = [];
-  this.dailyTotal = 0;
-  stores.push(this);
-  this.getHourlyCookies();
-}
-console.log(Store);
-
-Store.prototype.randomCust = function () {
-  return getRandomNum(this.min, this.max);
-};
-
-Store.prototype.getHourlyCookies = function() {
-  for(var i = 0 ; i < hours.length; i++) {
-    this.hourlyCookies.push(Math.floor(this.randomCust() * this.avg));
-    this.dailyTotal += this.hourlyCookies[i];
-  }
-  return this.getHourlyCookies;
-};
-
-new Store ('1st and Pike', 23, 65, 6.3);
-new Store ('SeaTac Airport', 3, 24, 1.2);
-new Store ('Seattle Center', 11, 38, 3.7);
-new Store ('Capitol Hill', 20, 38, 2.3);
-new Store ('Alki', 2, 26, 1.2);
-
-function getRandomNum(min, max) {
-  return Math.round(Math.random() * (max - min) + min);
-}
+// 'use strict';
+// var stores = [];
+//
+// var hours = ['6:00 AM', '7:00 AM', '8:00 AM', '9:00 AM', '10:00 AM','11:00 AM','12:00 PM','1:00 PM','2:00 PM','3:00 PM','4:00 PM','5:00 PM','6:00 PM','7:00 PM','8:00 PM','Total'];
+//
+// function Store (id, min, max, avg){
+//   this.id = id;
+//   this.min = min;
+//   this.max = max;
+//   this.avg = avg;
+//   this.hourlyCookies = [];
+//   this.dailyTotal = 0;
+//   stores.push(this);
+//   this.getHourlyCookies();
+// }
+// console.log(Store);
+//
+// Store.prototype.randomCust = function () {
+//   return getRandomNum(this.min, this.max);
+// };
+//
+// Store.prototype.getHourlyCookies = function() {
+//   for(var i = 0 ; i < hours.length; i++) {
+//     this.hourlyCookies.push(Math.floor(this.randomCust() * this.avg));
+//     this.dailyTotal += this.hourlyCookies[i];
+//   }
+//   return this.getHourlyCookies;
+// };
+//
+// new Store ('1st and Pike', 23, 65, 6.3);
+// new Store ('SeaTac Airport', 3, 24, 1.2);
+// new Store ('Seattle Center', 11, 38, 3.7);
+// new Store ('Capitol Hill', 20, 38, 2.3);
+// new Store ('Alki', 2, 26, 1.2);
+//
+// function getRandomNum(min, max) {
+//   return Math.round(Math.random() * (max - min) + min);
+// }
